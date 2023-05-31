eb6973546cbdc73d4394f4219033ab80c7959bbb
nicoprietog <nicolas.prieto@hotmail.es>
2023-05-31 13:42:33 -0500

    The change I made here was purely to switch the language from Spanish to English, as my project will be entirely written in English. I would also like to take this opportunity to explain what is inside the "location_df" file. The purpose here is to create a DataFrame where the columns represent the available aisles for storing products. Each aisle has locations, and there are a total of 20 locations per aisle. These locations serve as the indices of my DataFrame. The data inside shows three values: the current quantity of boxes in the location (which is zero because it's empty), the maximum quantity of boxes determined by the respective aisle, and the maximum quantity of boxes that can be stacked for safety reasons.

--- location_df.py
+++ location_df.py
@@ -1,35 +1,20 @@
 import pandas as pd
 
-# Generar nombres de columnas:
-num_pasillos = 50
-nombres_columnas = ['Pasillo ' + str(i) for i in range(1, num_pasillos + 1)]
-
-#Máximo por locación:
-distribucion_locaciones = {
-    "Hasta pasillo 15" : [20,2],
-    "De 16 a 40 " : [40,4],
-    "De 41 a 50" : [40,2]
-}
-
-# Generar datos para las tuplas:
-datos_tuplas = []
-for i in range(1, num_pasillos + 1):
-    #Cantidad máxima de cajas por locación:
+#Generate columns names:
+aisle_num = 50
+columns_names = ['Aisle ' + str(i) for i in range(1,aisle_num+1)]
+# Generate data for tuples:
+tuple_data = []
+for i in range(1, aisle_num + 1):
+    #Max boxes in a tower, depends the aisle:
     a = (20 if i <= 15 else (40 if i > 15 and i <= 40 else 48))
-    #Altura máxima de cajas por locación:
+    #Max height of boxes one over the other, also depends of the aisle:
     b = (2 if i <= 15 else (4 if i > 15 and i <= 40 else 2))
-    datos_tuplas.append((0,a,b))
-
-# Crear el DataFrame de tuplas
-df = pd.DataFrame([datos_tuplas], columns=nombres_columnas)
-
-#Agregar filas extras:
-num_filas_adicionales = 20
-df = pd.concat([df] * (num_filas_adicionales), ignore_index=True)
-print(df)
-
-
-#Locación seleccionada:
-
-select = df.loc[0,["Pasillo 1"]]
-print(select)
+    #Create a tuple with the last two data, and the cero thats works as the actual number of boxes (everything its empty):
+    tuple_data.append((0,a,b))
+# Create de DataFrame from the tuple_data:
+initial_locations_df = pd.DataFrame([tuple_data], columns=columns_names)
+#Add the extra locations per aisle:
+num_of_locations_per_aisle = 20
+final_locations_df = pd.concat([initial_locations_df] * (num_of_locations_per_aisle), ignore_index=True)
+print(final_locations_df)
