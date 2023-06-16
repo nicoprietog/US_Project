92c5dee2dd5d9f53567044b8cfd7f5a74d017f54
nicoprietog <nicolas.prieto@hotmail.es>
2023-06-16 18:30:06 -0500

    .

--- main.py
+++ main.py
@@ -24,14 +24,11 @@ def gen_random_location(type_products, locations_copy):
     random_locations = []
     for i in type_products:
         # Give me the number of aisle where the product can be saved:
-        aisles_available = ((list(range(0, 15)) if i == 1 else (list(range(15, 40)) if i == 2 else (
-            list(range(40, 50)) if i == 3 else print("This kind of product do not exist")))))
+        aisles_available = ((list(range(0, 15)) if i == 1 else (list(range(15, 40)) if i == 2 else (list(range(40, 50)) if i == 3 else print("This kind of product do not exist")))))
         # Bring the whole DF segment of the Locations DF:
         locations_for_search = locations_copy.iloc[:, aisles_available]
         # Create a function to give a random number depends the aisle (the determinate number is because each type of product have a space of X aisles to be allocated):
-        rand_aisle = random.randint(1, 15) if locations_for_search.shape[1] == 15 else (
-            random.randint(15, 40) if locations_for_search.shape[1] == 25 else (
-                random.randint(40, 50) if locations_for_search.shape[1] == 10 else print("error")))
+        rand_aisle = random.randint(1, 15) if locations_for_search.shape[1] == 15 else (random.randint(15, 40) if locations_for_search.shape[1] == 25 else (random.randint(40, 50) if locations_for_search.shape[1] == 10 else print("error")))
         rand_pos = random.randint(0, 19)
         rand_location = (rand_pos, rand_aisle)
         random_locations.append(rand_location)
@@ -40,42 +37,29 @@ def gen_random_location(type_products, locations_copy):
 
 # Call the function to generate random coordinates:
 random_locations = gen_random_location(type_products, locations_copy)
-
 # Generate a list with the quantities of orders:
 orders_quantity = orders["quantity"].tolist()
 
-print(random_locations)
+print("These are the random locations: ",random_locations)
 
+changes = []
 for i in random_locations:
-
-    row = random_locations[i][0]
-    col = random_locations[i][1]
+    #Here I take the coordinates of each random location, I divided by rows and columns:
+    row = i[0]
+    col = i[1]
+    #Find the total amount of free spaces at the random location (second number minus the first):
     available_spaces_location = locations_copy.at[row, f"aisle{col}"][1] - locations_copy.at[row, f"aisle{col}"][0]
-    pallets_to_allocate = orders_quantity[i]
+    #How many pallets needs to be allocated:
+    pallets_to_allocate = orders_quantity[random_locations.index(i)]
+    #Logical function to allocate pallets:
     if available_spaces_location > 0:
+        #In this case, there´s much pallets than the spaces available:
         if pallets_to_allocate > available_spaces_location:
+            changes.append([row,col,available_spaces_location])
             pallets_to_allocate -= available_spaces_location
-            available_spaces_location == 0
-
-        random_locations = gen_random_location(type_products, locations_copy)
-    else pallets_to_allocate == 0:
-        break
-
-    # if orders_quantity[i]:
-
-    print(available_spaces_location)
-
-# x = locations_copy.at[2,"aisle3"][3]
-# print(x)
-
-"""
-
-
-
-
-
-
-
- 
+        #Here, there are just a few pallets, less than the total spaces availables:
+        elif pallets_to_allocate < available_spaces_location:
+            changes.append([row, col,pallets_to_allocate])
+print(changes)
 
 
